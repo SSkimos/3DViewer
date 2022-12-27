1aee4064b7cb179b7eb8835d47dd027e5c050f15
Chereshnev <chereshnev.n.s@edu.mirea.ru>
2022-12-27 21:05:51 +0300

    :bug: mini fix

--- src/view/myglwidget.cpp
+++ src/view/myglwidget.cpp
@@ -1,6 +1,7 @@
 #include "myglwidget.h"
 #include "../model/s21_data_structure.h"
 #include "../model/s21_parse_obj.h"
+#include "../model/s21_affin_p.h"
 #include <iostream>
 
 MyGLWidget::MyGLWidget(QWidget *parent) : QOpenGLWidget(parent)
@@ -18,33 +19,16 @@ void MyGLWidget::initializeGL(void) {
     glEnable(GL_LIGHTING);
     glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
     glEnable(GL_COLOR_MATERIAL);
-    glBegin(GL_TRIANGLES);
-        glVertex3f(-0.5f, -0.5f, 0.0f);
-        glVertex3f( 0.5f,  0.5f, 0.0f);
-        glVertex3f(-0.5f,  0.5f, 0.0f);
-      glEnd();
 }
 
 void MyGLWidget::paintGL(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     data_t* s = ParseCountObj("../../../../model/obj/cube.obj");
     glLoadIdentity();
-    //glBegin(GL_TRIANGLES);
-    /*   // glColor3ub(255,0,0); // красный
-        glVertex3f(-0.5f, -0.5f, 0.0f);
-        glVertex3f( 0.5f,  0.0f, 0.0f);
-        //glColor3ub(0,0,255); // синий
-        glVertex3f(-0.5f , 0.5f, 0.0f);
-      glEnd();//Reset the current model observation matrix */
-    /* for (unsigned int i = 0; i < s->facets_count; i++) {
-        glBegin(GL_POINTS);
-        for (int j = 0; j < s->polygons->vertexes[i]; j++) {
-        } */
-   // glBegin(GL_POINTS);
-     // glColor3ub(255,0,0);
-     // glBegin(GL_POINTS);
-    //printf("\n\n\n%d",s->matrix_3d.rows);
+    // MatrixMove(&(s->matrix_3d), 10, kX);
+//    MatrixScale(&(s->matrix_3d), 0.1);
     glEnableClientState(GL_VERTEX_ARRAY);
+    RotationX(&(s->matrix_3d), 2);
     for (int i = 1; i < s->matrix_3d.rows; i++) {
         glVertexPointer(3, GL_FLOAT, 0, s->matrix_3d.matrix[i]);
         // glVertex3f(s->matrix_3d.matrix[i][0], s->matrix_3d.matrix[i][1], s->matrix_3d.matrix[i][2]);
