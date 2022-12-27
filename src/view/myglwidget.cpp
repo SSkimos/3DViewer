e8eb0cb52250635b68728b649deaa8d87d884def
artemiikulikov <aapqwhat@gmail.com>
2022-12-27 19:31:28 +0300

    :shirt: update(qt): add primitive 3d rendering

--- src/view/myglwidget.cpp
+++ src/view/myglwidget.cpp
@@ -1,4 +1,7 @@
 #include "myglwidget.h"
+#include "../model/s21_data_structure.h"
+#include "../model/s21_parse_obj.h"
+#include <iostream>
 
 MyGLWidget::MyGLWidget(QWidget *parent) : QOpenGLWidget(parent)
 {
@@ -8,18 +11,56 @@ MyGLWidget::MyGLWidget(QWidget *parent) : QOpenGLWidget(parent)
 void MyGLWidget::initializeGL(void) {
     float r, g, b, a = 1.0f;
     initializeOpenGLFunctions();
-    qColorToRGB(Qt::red, r, g, b);
+    qColorToRGB(Qt::white, r, g, b);
     glClearColor(r, g, b, a);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHT0);
     glEnable(GL_LIGHTING);
     glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
     glEnable(GL_COLOR_MATERIAL);
+    glBegin(GL_TRIANGLES);
+        glVertex3f(-0.5f, -0.5f, 0.0f);
+        glVertex3f( 0.5f,  0.5f, 0.0f);
+        glVertex3f(-0.5f,  0.5f, 0.0f);
+      glEnd();
 }
 
 void MyGLWidget::paintGL(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    data_t* s = ParseCountObj("../../../../model/obj/cube.obj");
+    glLoadIdentity();
+    //glBegin(GL_TRIANGLES);
+    /*   // glColor3ub(255,0,0); // красный
+        glVertex3f(-0.5f, -0.5f, 0.0f);
+        glVertex3f( 0.5f,  0.0f, 0.0f);
+        //glColor3ub(0,0,255); // синий
+        glVertex3f(-0.5f , 0.5f, 0.0f);
+      glEnd();//Reset the current model observation matrix */
+    /* for (unsigned int i = 0; i < s->facets_count; i++) {
+        glBegin(GL_POINTS);
+        for (int j = 0; j < s->polygons->vertexes[i]; j++) {
+        } */
+   // glBegin(GL_POINTS);
+     // glColor3ub(255,0,0);
+     // glBegin(GL_POINTS);
+    //printf("\n\n\n%d",s->matrix_3d.rows);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    for (int i = 1; i < s->matrix_3d.rows; i++) {
+        for (int j = 0; j < s->matrix_3d.columns; j++) {
+            glVertexPointer(3, GL_FLOAT, 0, s->matrix_3d.matrix[i]);
+            // glVertex3f(s->matrix_3d.matrix[i][0], s->matrix_3d.matrix[i][1], s->matrix_3d.matrix[i][2]);
+            std::string a = std::to_string(s->matrix_3d.matrix[i][0]) + " " + std::to_string(s->matrix_3d.matrix[i][1]) + " " + std::to_string(s->matrix_3d.matrix[i][2]);
+            std::cout << a  << "cycle" << std::endl;
+            glColor3ub(255,0,0);
+        }
+    }
+    glDrawArrays(GL_TRIANGLES,0,3);
+    //QTextStream(stdout) << "\n\n\nSSASDASdasddsfdfsdaf";
 
+
+
+    // Сначала расставляем точки на нашей поверхности. Для этого идём по matrix_3d  и выставляем точки в значениях x и y.
+    // Данная отрисовка точек будет иметь параллельную проекцию (вы можете данный пункт пропустить, но он необходим для бонусной части).
 }
 
 void MyGLWidget::resizeGL(int width, int height) {
--- src/view/untitled.pro
+++ src/view/untitled.pro
@@ -7,15 +7,18 @@ CONFIG += c++17
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+LIBS += -L ../model/matrix_t -ls21_matrix
 
 SOURCES += \
+../model/s21_parse_obj.c \
     myglwidget.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
     myglwidget.h \
-    mainwindow.h
+    mainwindow.h \
+    ../model/s21_parse_obj.h
 
 FORMS += \
     mainwindow.ui
