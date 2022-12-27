ef31a2a3ec2917df879e03bdb14428311514f0be
artemiikulikov <aapqwhat@gmail.com>
2022-12-27 15:26:52 +0300

    :bug: bug(view): fix build bug

--- src/view/glviewer.h
+++ src/view/glviewer.h
@@ -1,16 +1,19 @@
 #ifndef GLVIEWER_H
 #define GLVIEWER_H
 #include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QOpenGLContext>
 
-class GLViewer: public QOpenGLWidget
+class GLViewer: public QOpenGLWidget, public QOpenGLFunctions
 {
+    Q_OBJECT
 public :
     GLViewer(QWidget *parent) : QOpenGLWidget(parent) { };
 
 protected:
-    void initializeGL();
-    void paintGL();
-    void resizeGL(int width, int height);
+    void initializeGL() override;
+    void paintGL() override ;
+    void resizeGL(int width, int height) override;
 
 };
 
--- src/view/untitled.pro
+++ src/view/untitled.pro
@@ -9,12 +9,12 @@ CONFIG += c++17
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    glwidget.cpp \
+    glviewer.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
-    glwidget.h \
+    glviewer.h \
     mainwindow.h
 
 FORMS += \
