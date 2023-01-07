8c8088c8a81c8175e05c8feff510d91799c86f9f
Curt Rika <curtrika@at-l8.msk.21-school.ru>
2023-01-07 16:42:56 +0300

    :shirt: refactor without OOP

--- new_src/model/position.h
+++ new_src/model/position.h
@@ -1,30 +1,11 @@
 #ifndef POSITION_H
 #define POSITION_H
 
-class Position
-{
-public:
-    Position();
-    ~Position();
-
-    void setRotateX(int newValue);
-    void setRotateY(int newValue);
-    void setRotateZ(int newValue);
-    int getRotateX() const;
-    int getRotateY() const;
-    int getRotateZ() const;
-
-    void setMoveX(int newValue);
-    void setMoveY(int newValue);
-    void setMoveZ(int newValue);
-    int getMoveX() const;
-    int getMoveY() const;
-    int getMoveZ() const;
-
-private:
+struct position {
     int rotateX;
     int rotateY;
     int rotateZ;
+
     int moveX;
     int moveY;
     int moveZ;
