3edc7436a38e9f63775e8c293b17b7193009d948
artemiikulikov <aapqwhat@gmail.com>
2023-01-07 18:19:53 +0300

    :star: update(all): CMAKE BUILD SYSTEM INIT

--- src/view/myglwidget.h
+++ src/view/myglwidget.h
@@ -19,7 +19,7 @@
 #include <QOpenGLFunctions>
 #include <QOpenGLShaderProgram>
 #include <QOpenGLVertexArrayObject>
-#include <glu.h>
+// #include <glu.h>
 
 class MyGLWidget: public QOpenGLWidget
 {
@@ -28,31 +28,31 @@ public :
     MyGLWidget(QWidget *parent = nullptr);
 
 protected:
-    void initializeGL() override;
-    void paintGL() override;
-    void resizeGL(int width, int height) override;
+   void initializeGL() override;
+   void paintGL() override;
+   void resizeGL(int width, int height) override;
 
-    void mousePressEvent(QMouseEvent *) override; // вызывается при нажатии мышки
-    void mouseMoveEvent(QMouseEvent *) override; // вызывается при движении мышки
+   void mousePressEvent(QMouseEvent *) override; // вызывается при нажатии мышки
+   void mouseMoveEvent(QMouseEvent *) override; // вызывается при движении мышки
 
 private:
-    float xRot, yRot, zRot;
-    QPoint mPos;
-    QTimer tmr;
-    void drawCube(float a);
+   float xRot, yRot, zRot;
+   QPoint mPos;
+   QTimer tmr;
+   void drawCube(float a);
 
  public:
-    double scale;
-    QOpenGLShaderProgram *prog = nullptr;
-    QOpenGLShaderProgram *initialize_shaders();
-    void add_example();
-    int vertex_count;
-    int lines_count;
-    float *vertex_array;
-    unsigned int *lines_array;
-    QVector3D lineColorV = {1, 1, 1};
-    QVector3D verticleColorV = {1, 0, 0};
-    int m_coeffMatrixLoc = 0;
+   double scale;
+   QOpenGLShaderProgram *prog = nullptr;
+   QOpenGLShaderProgram *initialize_shaders();
+   void add_example();
+   int vertex_count;
+   int lines_count;
+   float *vertex_array;
+   unsigned int *lines_array;
+   QVector3D lineColorV = {1, 1, 1};
+   QVector3D verticleColorV = {1, 0, 0};
+   int m_coeffMatrixLoc = 0;
 
 };
 
