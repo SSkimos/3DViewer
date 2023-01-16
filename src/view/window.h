16b0720bf263319c6ba5cf32b73415691b302438
Chereshnev <chereshnev.n.s@edu.mirea.ru>
2023-01-16 18:08:54 +0300

    :bug: bug with GIF

--- src/view/window.h
+++ src/view/window.h
@@ -7,6 +7,7 @@
 #include <QMainWindow>
 #include <QFileDialog>
 #include <QSettings>
+#include "../gif/gif.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Window; }
@@ -38,5 +39,14 @@ private:
     void SetMoveX(QSettings *settings); //TODO: не нравится нейминг
     void SetMoveY(QSettings *settings);
     void SetMoveZ(QSettings *settings);
+
+    void oneGif();
+    void createGif();
+
+    const int GifFps = 10, GifLength = 5;
+    QTimer *timer;
+    int startTime, tmpTime;
+    int counter = 1;
+
 };
 #endif // WINDOW_H
